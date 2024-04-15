44d80c56eb39f6bd0cf57d0de0b3e3c7049f855f
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-04-15 10:49:23 +0700

    video 26

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,6 +1,8 @@
 package vn.hoidanit.jobhunter.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
@@ -14,16 +16,13 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/create")
-    public String createNewUser() {
+    // @GetMapping("/user/create")
+    @PostMapping("/user/create")
+    public User createNewUser(
+            @RequestBody User postManUser) {
 
-        User user = new User();
-        user.setEmail("hoidanit@gmail.com");
-        user.setName("Eric");
-        user.setPassword("123456");
+        User ericUser = this.userService.handleCreateUser(postManUser);
 
-        this.userService.handleCreateUser(user);
-
-        return "create user";
+        return ericUser;
     }
 }

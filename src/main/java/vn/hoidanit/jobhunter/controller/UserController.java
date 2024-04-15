e9ceaf18de2b24b19df1538011a786211bd0eb06
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-04-15 21:06:38 +0700

    video 28

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,6 +1,8 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +18,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    // @GetMapping("/user/create")
-    @PostMapping("/user/create")
+    @PostMapping("/user")
     public User createNewUser(
             @RequestBody User postManUser) {
 
@@ -25,4 +26,11 @@ public class UserController {
 
         return ericUser;
     }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "ericUser";
+    }
+
 }

fea64993802d439298ecc98f08e8ac2a7eb53d11
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-04-15 21:45:40 +0700

    video 32

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,6 +46,12 @@ public class UserController {
     @GetMapping("/user")
     public List<User> getAllUser() {
         return this.userService.fetchAllUser();
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user) {
+        User ericUser = this.userService.handleUpdateUser(user);
+        return ericUser;
     }
 
 }

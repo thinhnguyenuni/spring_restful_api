4a0b1d4464728aec286e5a4323ad7d9c43624f5f
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-04-23 15:21:11 +0700

    video 41

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -31,11 +31,6 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(ericUser);
     }
 
-    @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<String> handleIdException(IdInvalidException idException) {
-        return ResponseEntity.badRequest().body(idException.getMessage());
-    }
-
     @DeleteMapping("/users/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable("id") long id)
             throws IdInvalidException {

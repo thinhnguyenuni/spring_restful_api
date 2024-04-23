c21f0026af9589ad9c4ffbff63568f2546b348c2
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-04-23 10:17:18 +0700

    video 39

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -3,9 +3,9 @@ package vn.hoidanit.jobhunter.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import vn.hoidanit.jobhunter.service.error.IdInvalidException;
 
 @RestController
 public class UserController {
@@ -30,8 +31,18 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(ericUser);
     }
 
+    @ExceptionHandler(value = IdInvalidException.class)
+    public ResponseEntity<String> handleIdException(IdInvalidException idException) {
+        return ResponseEntity.badRequest().body(idException.getMessage());
+    }
+
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable("id") long id) {
+    public ResponseEntity<String> deleteUser(@PathVariable("id") long id)
+            throws IdInvalidException {
+        if (id >= 1500) {
+            throw new IdInvalidException("Id khong lon hown 1501");
+        }
+
         this.userService.handleDeleteUser(id);
         return ResponseEntity.ok("ericUser");
         // return ResponseEntity.status(HttpStatus.OK).body("ericUser");

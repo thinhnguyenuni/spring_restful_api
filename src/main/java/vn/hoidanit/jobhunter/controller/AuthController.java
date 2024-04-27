2abb3b6a2a9931b6d04d705f6e173a7d7d16a05f
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-04-27 18:52:57 +0700

    video 53

--- src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.dto.LoginDTO;
 
 @RestController
@@ -20,7 +21,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> login(@RequestBody LoginDTO loginDto) {
+    public ResponseEntity<LoginDTO> login(@Valid @RequestBody LoginDTO loginDto) {
         // Nạp input gồm username/password vào Security
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 loginDto.getUsername(), loginDto.getPassword());
--- src/main/java/vn/hoidanit/jobhunter/service/UserDetailsCustom.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserDetailsCustom.java
@@ -8,18 +8,23 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import vn.hoidanit.jobhunter.util.error.IdInvalidException;
+
 @Component("userDetailsService")
-public class UserDetailCustom implements UserDetailsService {
+public class UserDetailsCustom implements UserDetailsService {
 
     private final UserService userService;
 
-    public UserDetailCustom(UserService userService) {
+    public UserDetailsCustom(UserService userService) {
         this.userService = userService;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         vn.hoidanit.jobhunter.domain.User user = this.userService.handleGetUserByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Username/password không hợp lệ");
+        }
 
         return new User(
                 user.getEmail(),

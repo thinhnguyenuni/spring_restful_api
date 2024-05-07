14864a30c5e2043c59a838126b90a6ff8a94c28b
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-05-07 15:29:48 +0700

    video 73

--- src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
@@ -1,6 +1,9 @@
 package vn.hoidanit.jobhunter.controller;
 
-import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,10 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.domain.dto.ResultPaginationDTO;
 import vn.hoidanit.jobhunter.service.CompanyService;
 
 @RestController
@@ -30,9 +35,17 @@ public class CompanyController {
     }
 
     @GetMapping("/companies")
-    public ResponseEntity<List<Company>> getCompany() {
-        List<Company> companies = this.companyService.handleGetCompany();
-        return ResponseEntity.ok(companies);
+    public ResponseEntity<ResultPaginationDTO> getCompany(
+            @RequestParam("current") Optional<String> currentOptional,
+            @RequestParam("pageSize") Optional<String> pageSizeOptional) {
+        String sCurrent = currentOptional.isPresent() ? currentOptional.get() : "";
+        String sPageSize = pageSizeOptional.isPresent() ? pageSizeOptional.get() : "";
+
+        int current = Integer.parseInt(sCurrent);
+        int pageSize = Integer.parseInt(sPageSize);
+
+        Pageable pageable = PageRequest.of(current - 1, pageSize);
+        return ResponseEntity.ok(this.companyService.handleGetCompany(pageable));
     }
 
     @PutMapping("/companies")

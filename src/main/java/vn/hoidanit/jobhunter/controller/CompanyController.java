7ea79f45fc0b9b30e200bd25db82b866eceed7d0
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-05-18 21:47:09 +0700

    video 106

--- src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
@@ -54,5 +56,12 @@ public class CompanyController {
     public ResponseEntity<Void> deleteCompany(@PathVariable("id") long id) {
         this.companyService.handleDeleteCompany(id);
         return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/companies/{id}")
+    @ApiMessage("fetch company by id")
+    public ResponseEntity<Company> fetchCompanyById(@PathVariable("id") long id) {
+        Optional<Company> cOptional = this.companyService.findById(id);
+        return ResponseEntity.ok().body(cOptional.get());
     }
 }

28210077db2db51c865267d791d56edc43b9eaee
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-05-31 12:55:18 +0700

    video 125

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -20,7 +20,11 @@ public class EmailController {
     @GetMapping("/email")
     @ApiMessage("Send simple email")
     public String sendSimpleEmail() {
-        this.emailService.sendSimpleEmail();
+        // this.emailService.sendSimpleEmail();
+        // this.emailService.sendEmailSync("ads.hoidanit@gmail.com", "test send email",
+        // "<h1> <b> hello </b> </h1>", false,
+        // true);
+        this.emailService.sendEmailFromTemplateSync("ads.hoidanit@gmail.com", "test send email", "job");
         return "ok";
     }
 }

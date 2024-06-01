444c513757c1a040396e1a7b463bf044b9674cd9
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-06-01 17:32:38 +0700

    video 129

--- src/main/java/vn/hoidanit/jobhunter/service/SubscriberService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/SubscriberService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.Job;
@@ -32,6 +33,11 @@ public class SubscriberService {
         this.jobRepository = jobRepository;
         this.emailService = emailService;
     }
+
+    // @Scheduled(cron = "*/10 * * * * *")
+    // public void testCron() {
+    // System.out.println(">>> TEST CRON");
+    // }
 
     public boolean isExistsByEmail(String email) {
         return this.subscriberRepository.existsByEmail(email);

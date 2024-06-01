a120793e73fe2a0492e2f99888ed60982325dab1
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-06-01 19:35:49 +0700

    video 130

--- src/main/java/vn/hoidanit/jobhunter/service/SubscriberService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/SubscriberService.java
@@ -112,4 +112,8 @@ public class SubscriberService {
             }
         }
     }
+
+    public Subscriber findByEmail(String email) {
+        return this.subscriberRepository.findByEmail(email);
+    }
 }
